33030c0ae0f3d370247f3ba72701d60b40fc4110
Jaime Lima Neto <jaimeflneto@jaimeflneto.local>
2026-05-28 19:57:56 -0300

    Retornando ao tracking do git o index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // conectar com o banco de dados
+    // fazer validações
+    console.log('req', req.query)
+    const name = req.query.name;
+    if (!name) {
+        return res.status(400).send('Name is required');
+    }
+    res.send(`Oi, ${name}`);
+});
+
+// teste 
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
